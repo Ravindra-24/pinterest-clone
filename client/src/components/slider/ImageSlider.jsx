71aaa80c42562e3680aee1bcae32b3c6942c9c0c
Ravindra Pawar <ravindra.pawar.mit@gmail.com>
2024-02-23 10:39:00 +0530

    bug fix in imageSlider and galleri component

--- client/src/components/slider/ImageSlider.jsx
+++ client/src/components/slider/ImageSlider.jsx
@@ -34,19 +34,19 @@ const ImageSlider = () => {
         indicatorStyle={indicatorStyles} // Add indicatorStyle prop
       >
         {slideImages &&
-          slideImages.map((image) => (
-            <div className="each-slide-effect" key={image._id}>
+          slideImages.map((images) => (
+            <div className="each-slide-effect" key={images._id}>
               <div
                 className="background-img"
-                style={{ backgroundImage: `url(${image.image})` }}
+                style={{ backgroundImage: `url('${images.image}')` }} // Ensure the URL is properly formatted
               >
                 <div className="slider-desc">
-                  <span>{image.title}</span>
-                  <p>{image.description}</p>
+                  <span>{images.title}</span>
+                  <p>{images.description}</p>
 
                   <button
                     className="max-w-min text-white focus:ring-4 shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none  active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-gradient-to-r from-pink-500 to-yellow-500 hover:from-pink-600 hover:to-yellow-600"
-                    onClick={() => navigate(`/post/${image._id}`)}
+                    onClick={() => navigate(`/post/${images._id}`)}
                   >
                     View
                   </button>
@@ -61,4 +61,3 @@ const ImageSlider = () => {
 };
 
 export default withPublic(ImageSlider);
- 
